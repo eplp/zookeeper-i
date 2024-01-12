4a919a5d781f42babde57694b6be2a1edc4d844e
eplp <edgardopina57@gmail.com>
2024-01-11 20:32:31 -0700

    Added starter code for frontend. added static path directory for public. Added these routes: '/' - root, '/animals', '/zookeepers', woldcard route '*'

--- server.js
+++ server.js
@@ -7,24 +7,30 @@ const { animals } = require('./data/animals.json');
 
 const app = express(); //* assign express to "app" to chain express server methods
 
-//! express.js MIDDLEWARE to parse incoming (to the server) data
+//! express.js MIDDLEWARE
+
+//* Middleware to parse incoming(to the server) data
 //! Middleware functions can serve many different purposes. Ultimately they allow us to keep our
 //! route endpoint callback functions more readable while letting us reuse functionality across
 //! routes to keep our code DRY.
 //* intercept our POST request before it gets to the callback function. At that point,
 //* the data will be run through a couple of functions to take the raw data transferred over
 //* HTTP and convert it to a JSON object.
-
-app.use(express.urlencoded({ extended: true })); //* parse incoming string or array data
-app.use(express.json()); //* parse incoming JSON data
-
 //* The express.urlencoded({extended: true}) method is a method built into Express.js. It takes incoming POST data
 //* and converts it to key / value pairings that can be accessed in the req.body object.The extended: true
 //* option set inside the method call informs our server that there may be sub - array data nested in it as well,
 //* so it needs to look as deep into the POST data as possible to parse all of the data correctly.
+//* The express.json() method we use takes incoming POST data in the form of JSON and parses it into the req.body JavaScript object.
+//! Both of the following middleware functions need to be set up every time you create a server that's looking to accept POST data.
+app.use(express.urlencoded({ extended: true })); //* parse incoming string or array data
+app.use(express.json()); //* parse incoming JSON data
 
-//* The express.json() method we used takes incoming POST data in the form of JSON and parses it into the req.body JavaScript object.
-//! Both of the above middleware functions need to be set up every time you create a server that's looking to accept POST data.
+//* Middleware to instruct the server to make certain files readily available and to
+//* not gate it behind a server endpoint.
+//* provide a file path to a location in our application (in this case, the public folder) and instruct the server to make these files static resources. This means that all of our front-end code can now be accessed without having a specific server endpoint created for it!
+app.use(express.static('public'));
+
+//! *************** end of middleware  *************
 
 const PORT = process.env.PORT || 3001;
 
@@ -69,7 +75,7 @@ const createNewAnimal = (body, animalsArray) => {
    return body;
 };
 
-function validateAnimal(animal) {
+const validateAnimal = (animal) => {
    if (!animal.name || typeof animal.name !== 'string') {
       return false;
    }
@@ -83,9 +89,9 @@ function validateAnimal(animal) {
       return false;
    }
    return true;
-}
+};
 
-//! general API - GET
+//? general API - GET
 //* uses req.query
 app.get('/api/animals', (req, res) => {
    let results = animals; //* "loads" data (animals) into working variable
@@ -97,8 +103,7 @@ app.get('/api/animals', (req, res) => {
    res.json(results); //* 'returns' api call results
 });
 
-//! param API route - GET
-
+//? param API route - GET
 //! This route MUST be AFTER the general api route
 //* - uses req.params.<property-name> - The req object gives us access to another property for
 //* this case, req.params.Unlike the query object, the param object needs to be
@@ -112,7 +117,7 @@ app.get('/api/animals/:id', (req, res) => {
    }
 });
 
-//! POST
+//? POST
 //* uses req.body
 app.post('/api/animals', (req, res) => {
    req.body.id = animals.length.toString(); //* creates next
@@ -124,6 +129,29 @@ app.post('/api/animals', (req, res) => {
    }
 });
 
+//? GET - Root server route - renders index.html
+app.get('/', (req, res) => {
+   res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+//? GET - renders animals.html
+app.get('/animals', (req, res) => {
+   res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+//? GET - renders zookeepers.html
+app.get('/zookeepers', (req, res) => {
+   res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+//? Wildcard route
+//! MUST be always last route
+//*  any route that wasn't previously defined will fall under this request and will receive the homepage as the response. 
+app.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+//! ALWAYS the last express app method
 app.listen(PORT, () => {
    console.log(`API server now on port ${PORT}!`);
 });
